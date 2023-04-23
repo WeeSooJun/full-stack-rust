9cfd9c87d3fffc810e0477db64838aac0a8a4608
DESKTOP-ENFJ56U\weesj <weesj96@gmail.com>
2023-04-23 23:23:01 +0800

    Add query params

--- routing/src/routes/mod.rs
+++ routing/src/routes/mod.rs
@@ -2,6 +2,7 @@ mod hello_world;
 mod mirror_body_json;
 mod mirror_body_string;
 mod path_variables;
+mod query_params;
 
 use axum::{
     routing::{get, post},
@@ -11,6 +12,7 @@ use hello_world::hello_world;
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
 use path_variables::{hard_coded_path, path_variables};
+use query_params::query_params;
 
 pub fn create_routes() -> Router {
     Router::new()
@@ -19,4 +21,5 @@ pub fn create_routes() -> Router {
         .route("/mirror_body_json", post(mirror_body_json))
         .route("/path_variables/15", get(hard_coded_path)) // Order of calling routes for path variable does not matter, the library picks the one that is a better match.
         .route("/path_variables/:id", get(path_variables))
+        .route("/query_params", get(query_params))
 }
