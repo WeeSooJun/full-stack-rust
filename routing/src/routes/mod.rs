d47d3db60370b9568a58fb10040be13b36ccf790
DESKTOP-ENFJ56U\weesj <weesj96@gmail.com>
2023-04-23 22:51:41 +0800

    Add path variables

--- routing/src/routes/mod.rs
+++ routing/src/routes/mod.rs
@@ -1,14 +1,22 @@
 mod hello_world;
-mod mirror_body_string;
 mod mirror_body_json;
+mod mirror_body_string;
+mod path_variables;
 
-use axum::{routing::{get, post}, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 use hello_world::hello_world;
-use mirror_body_string::mirror_body_string;
 use mirror_body_json::mirror_body_json;
+use mirror_body_string::mirror_body_string;
+use path_variables::{hard_coded_path, path_variables};
 
 pub fn create_routes() -> Router {
-    Router::new().route("/", get(hello_world))
+    Router::new()
+        .route("/", get(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
         .route("/mirror_body_json", post(mirror_body_json))
+        .route("/path_variables/15", get(hard_coded_path)) // Order of calling routes for path variable does not matter, the library picks the one that is a better match.
+        .route("/path_variables/:id", get(path_variables))
 }
